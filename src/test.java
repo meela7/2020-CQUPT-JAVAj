81a9eacc36cd0077e724e531fd9f5898a662a0c9
pisces <pisces@192.168.31.101>
2020-02-24 20:57:10 +0900

    First Commit

---
+++ src/test.java
@@ -0,0 +1,17 @@
+
+/**
+ * @author pisces
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+    
+	}
+
+}
